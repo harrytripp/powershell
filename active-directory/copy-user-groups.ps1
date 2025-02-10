5d4fa5a6aabec176e5d256a88d303ee8b3e3f7d4
Harry Tripp <115900915+harrytripp@users.noreply.github.com>
2025-02-10 11:42:31 +0000

    Syntax updates

--- active-directory/copy-user-groups.ps1
+++ active-directory/copy-user-groups.ps1
@@ -1,13 +1,13 @@
 # Only use if in same OU to avoid complicating inherited membership
+# Must be ran as admin
 
 $reference = "j.doe"
 $copy = "b.gates"
-
 $groups = Get-ADPrincipalGroupMembership $reference | Select-Object SamAccountName
 $ExistingGroups = Get-ADPrincipalGroupMembership $copy | Select-Object SamAccountName
-$MissingGroups = $ExistingGroups| ?{$groups -notcontains $_}
+$MissingGroups = $groups| Where-Object{$ExistingGroups -notcontains $_}
 
-Write-Host 'You are about to add user"$copy" to these groups:'
+Write-Host 'You are about to add user'$copy' to these groups:'
 Write-Host $MissingGroups
 $check = Read-Host 'Would you like to continue? y/N'
 
