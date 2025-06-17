a7df39c470da75190820998548b79a81f893ce48
Harry <harryjtripp@gmail.com>
2025-06-17 16:19:55 +0100

    Create config-msgraph.ps1

---
+++ utilities/config-msgraph.ps1
@@ -0,0 +1,8 @@
+# Install
+Install-Module Microsoft.Graph -Scope CurrentUser <#or AllUsers#> -Repository PSGallery -Force
+
+# Verify
+Get-InstalledModule Microsoft.Graph
+
+# Update
+Update-Module Microsoft.Graph
