5bfbf43056a78c6bc6d99fb6c48f248d0cb13c85
Harry <harryjtripp@gmail.com>
2025-07-01 10:17:31 +0100

    Create graph\experiments and add edited msgraph tutorial

---
+++ graph/experiments/msgraph-training-powershell/graphtutorial/GraphTutorial.ps1
@@ -0,0 +1,71 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+Write-Host 'PowerShell Graph Tutorial'
+
+# Load settings
+$settings = Get-Content 'C:\VSCode\labs\msgraph-training-powershell\graphtutorial\settings.json' -ErrorAction Stop | Out-String | ConvertFrom-Json
+
+$clientId = $settings.clientId
+$authTenant = $settings.authTenant
+$graphScopes = $settings.graphUserScopes
+
+# <UserAuthSnippet>
+# Authenticate the user
+
+# HT 27/06/25: -ContextScope Process is workaround for Auth bug - https://github.com/microsoftgraph/msgraph-sdk-powershell/issues/3197#issuecomment-2896474896
+Connect-MgGraph -ClientId $clientId -TenantId $authTenant -Scopes $graphScopes -UseDeviceAuthentication -ContextScope Process -NoWelcome
+# </UserAuthSnippet>
+
+# <GetContextSnippet>
+# Get the Graph context
+#Get-MgContext
+# </GetContextSnippet>
+
+# <SaveContextSnippet>
+$context = Get-MgContext
+# </SaveContextSnippet>
+
+# <GetUserSnippet>
+# Get the authenticated user by UPN
+$user = Get-MgUser -UserId $context.Account -Select 'displayName, id, mail, userPrincipalName'
+# </GetUserSnippet>
+
+# <GreetUserSnippet>
+Write-Host "Hello," $user.DisplayName
+# For Work/school accounts, email is in Mail property
+# Personal accounts, email is in UserPrincipalName
+Write-Host "Email:", ($user.Mail ?? $user.UserPrincipalName)
+# </GreetUserSnippet>
+
+# <GetInboxSnippet>
+Get-MgUserMailFolderMessage -UserId $user.Id -MailFolderId Inbox -Select `
+  "from,isRead,receivedDateTime,subject" -OrderBy "receivedDateTime DESC" `
+  -Top 25 | Format-Table Subject,@{n='From';e={$_.From.EmailAddress.Name}}, `
+  IsRead,ReceivedDateTime
+# </GetInboxSnippet>
+
+# <DefineMailSnippet>
+$sendMailParams = @{
+    Message = @{
+        Subject = "Testing Microsoft Graph"
+        Body = @{
+            ContentType = "text"
+            Content = "Hello world!"
+        }
+        ToRecipients = @(
+            @{
+                EmailAddress = @{
+                    Address = ($user.Mail ?? $user.UserPrincipalName)
+                }
+            }
+        )
+    }
+}
+# </DefineMailSnippet>
+
+# <SendMailSnippet>
+Send-MgUserMail -UserId $user.Id -BodyParameter $sendMailParams
+# </SendMailSnippet>
+
+Disconnect-MgGraph
