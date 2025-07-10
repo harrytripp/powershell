7d8d8e79603b0a63104c29ced1ab785e82bf4547
Harry <harryjtripp@gmail.com>
2025-07-10 13:19:41 +0100

    Fixed computer removal step and improved logging

--- active-directory/change/remove-computers-stale-logging.ps1
+++ active-directory/change/remove-computers-stale-logging.ps1
@@ -1,10 +1,11 @@
 # For manual or scheduled use.
 # Targets: Only explicitly included OUs. Computers not logged into in over 6 months.
-# Exludes: whitelisted computers.
+# Exludes: Whitelisted computers.
 
 $rundate = (get-date).ToString("dd_MMMM_yyyy_H_mm_ss")
 $logpath = ".\logfiles\errors\ErrorLog_$($rundate).log"
 $csvpath = ".\logfiles\computers\StaleComputers_$($rundate).csv"
+$rempath = ".\logfiles\computers\RemovedComputers_$($rundate).log"
 $parentdir = @(".\logfiles\errors",".\logfiles\computers")
 
 # Check if logging parent directories exist and silently create if not.
@@ -49,11 +50,9 @@ $geterrors = $getcomputers | Where-Object { $_.IsError }
 $computers = $getcomputers | Where-Object { -not $_.IsError }
 
 # Log errors related to getting computers
-
 foreach ($err in $geterrors) {
     Add-Content -Path $logpath -Value "$((Get-Date).ToString("dd MMMM yyyy H:mm:ss")) - [AD Query Error]`n- OU: $($err.OU)`n- Message: $($err.Message)`n- Line: $($err.Line)`n- Position: $($err.Position)`n- StackTrace: $($err.StackTrace)`n`n===== END OF AD QUERY ERROR =====`n`n" -Force
 }
-
 
 # Export successfully returned computers and relevant properties
 $computers | Select-Object name, sAMAccountName, lastLogon, lastLogonDate, lastLogonTimestamp, whenCreated, objectSid, distinguishedName, Description | Export-Csv -Path $csvpath -NoTypeInformation
@@ -61,8 +60,8 @@ $computers | Select-Object name, sAMAccountName, lastLogon, lastLogonDate, lastL
 # Try to remove each successfully returned computer
 Foreach ($computer in $computers) {
     try {
-        NonsenseString
-        #Remove-ADComputer -WhatIf -Identity $_.sAMAccountName | Remove-ADObject -WhatIf -Recursive
+        Remove-ADComputer -Identity $computer.sAMAccountName -Confirm:$false -WhatIf
+        Add-Content -Path $rempath -Value "$((get-date).ToString("dd MMMM yyyy H:mm:ss")) $($computer.sAMAccountName) was successfully removed." -Force
     }
     catch {
         Add-Content -Path $logpath -Value "$((get-date).ToString("dd MMMM yyyy H:mm:ss")) - [Remove-Computer Error]`n- Computer: $($computer.sAMAccountName)`n- Exception Message: $($_.Exception.Message)`n- Line: $($_.InvocationInfo.Line)`n- Position: $($_.InvocationInfo.PositionMessage)`n- StackTrace: $($_.ScriptStackTrace)`n`n===== END OF REMOVE COMPUTER ERROR =====`n`n" -Force
